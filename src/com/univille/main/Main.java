c934635e7eb7d7d68a06c64fc8cfb86eca8fc46f
Eduardo Gabriel <eduardofranciscogabriel@gmail.com>
2016-04-15 12:26:27 -0300

    teste

--- src/com/univille/main/Main.java
+++ src/com/univille/main/Main.java
@@ -1,10 +1,21 @@
 package com.univille.main;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    System.out.print("teste");
+    public static void main(String[] args) throws IOException {
 
+
+        for(String line : Files.readAllLines(Paths.get("document//definitions.txt"))){
+
+            for(String part : line.split((" "))){
+                System.out.println(part);
+            }
+
+        }
 
     }
 }
