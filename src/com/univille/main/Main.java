01675547a5c3d910350ce5ef7a6afec428444744
Eduardo Gabriel <eduardofranciscogabriel@gmail.com>
2016-05-03 09:50:15 -0300

    final version

--- src/com/univille/main/Main.java
+++ src/com/univille/main/Main.java
@@ -1,50 +1,90 @@
 package com.univille.main;
 
-import com.univille.afd.Automata;
-import com.univille.afd.State;
-import com.univille.afd.Transition;
-
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 
 public class Main {
+        private String activeState;
+        private String initSate;
 
     public static void main(String[] args) throws IOException {
 
-        int lineCount = 0;
-
+        List<String> file = Files.readAllLines(Paths.get("document//definitions.txt"));
+        List<String> test = Files.readAllLines(Paths.get("document//test.txt"));
         HashMap<String,String> states = new HashMap<>();
         HashMap<String,String> words = new HashMap<>();
+        HashMap<String,String> end = new HashMap<>();
+        String[][] matrix = null;
+        Main automato = new Main();
 
+        for(int line = 0; line < file.size();line++){
+            int i;
 
-        for(String line : Files.readAllLines(Paths.get("document//definitions.txt"))){
-
-            for(int i = 0;i < line.split(" ").length;i++){
-
-                //System.out.println("value: "+line.split(" ")[i]);
-                if(lineCount == 0){
-                    states.put(line.split(" ")[i],String.valueOf(i));
+            if(line == 0){
+                String[] split = file.get(line).split(" ");
+                for(i =0;i < split.length;i++){
+                    //System.out.println("add state: " + split[i]);
+                    states.put(split[i],String.valueOf(i));
                 }
-
-                else if(lineCount == 1){
-                    words.put(line.split(" ")[i],String.valueOf(i));
-                }
-
-                else if(lineCount > 1 && lineCount < line.length()-2){
-
-                }
-
             }
 
+            else if(line == 1){
+                String[] split = file.get(line).split(" ");
+                for(i =0;i < split.length;i++){
+                    //System.out.println("add words: " + split[i]);
+                    words.put(split[i],String.valueOf(i));
+                }
+                matrix = new String[states.size()][words.size()];
+            }
 
-            lineCount++;
+            else if(line > 1 && line <file.size()-2){
+                String[] split = file.get(line).split(" ");
+                for(i =0;i < split.length;i++){
+                    //System.out.println("Linha: "+(line-2)+ " coluna: "+i+" add: " +split[i]);
+                    matrix[line-2][i] = split[i];
+                }
+            }
+
+            else if(line == file.size()-2){
+                automato.activeState = file.get(line);
+                automato.initSate = file.get(line);
+            }
+
+            else if(line == file.size()-1){
+                String[] split = file.get(line).split(" ");
+                for(i =0;i < split.length;i++){
+                    //System.out.println("end words: " + split[i]);
+                    end.put(split[i],String.valueOf(i));
+                }
+            }
         }
 
 
-        System.out.print(words.get("1"));
+        FileWriter fileResponse = new FileWriter("document//result.txt");
+        PrintWriter response = new PrintWriter(fileResponse);
+
+        for(int line = 0; line < test.size();line++){
+            String[] split = test.get(line).split(" ");
+            for(int i =0;i < split.length;i++){
+                automato.activeState = matrix[Integer.parseInt(states.get(automato.activeState))][Integer.parseInt(split[i])];
+               // System.out.println("Recebeu: "+split[i]+" Foi para: "+ automato.activeState);
+            }
+            if(end.get(automato.activeState) == null){
+                response.printf("Linha "+(line+1)+": "+"Recusado%n");
+                System.out.println("Recusado");
+            }
+            else{
+                response.printf("Linha "+(line+1)+": "+"Aceito%n");
+                System.out.println("Aceito");
+            }
+            automato.activeState = automato.initSate;
+        }
+
+        fileResponse.close();
     }
 }
